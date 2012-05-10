eb26b1c2574861d80ff8806957c11a98c5634eff
Keith Feldman <keithfeldman@mail.rit.edu>
2012-05-10 15:36:02 -0400

    Input works using args.

--- Blowfish.cpp
+++ Blowfish.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <cmath>
 #include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -9,7 +12,7 @@ using namespace std;
 void print_uint8_hex(uint8_t* buffer, uint32_t size, const char* label) {
     cout << label << hex;
     for ( unsigned int i = 0; i < size; i++ ) {
-         cout << (uint32_t) buffer[ i ] << " ";
+        cout << (uint32_t) buffer[ i ] << " ";
     }
     cout << dec << endl;
 }
@@ -17,40 +20,48 @@ void print_uint8_hex(uint8_t* buffer, uint32_t size, const char* label) {
 int main( int argc, char* argv[] ){
     Blowfish b;
 
+    if( (unsigned int) argc != b.blockSize() + b.keySize() + 1 ){
+        cout << "usage: Blowfish key[ 0th byte ] key[ 1st byte ] " \
+                "etc... ptext[ 0th byte ] ptext[ 1st byte ] etc..." << endl;
+        exit( 0 );
+    }
+    
+    for( int i = 1; i < argc; i++ ){
+        if( strlen( argv[ i ] ) != 2 ){
+            cout << "This arg isn't 2 characters long." << endl;
+            exit( 0 );
+        }
+    }
+        
     uint8_t* key = new uint8_t[ b.keySize() ];
     
     for( unsigned int i = 0; i < b.keySize(); i++ ){
-        key[ i ] = 0xFF;
+        char * p;
+        key[ i ] = (uint8_t) strtol( argv[ 1 + i ], &p, 16 );
+        if( *p != 0 ){
+            cout << "Problem parsing string." << endl;
+        }
     }
     
     print_uint8_hex(key, b.keySize(), "Key: ");
     b.setKey( key );
     
+
+    uint8_t* text = new uint8_t[ b.blockSize() ];
+
     
-    uint8_t* long_text = new uint8_t[ b.blockSize() * 2 ];
-    for( unsigned int i = 0; i < ( b.blockSize() * 2 ); i++ ){
-        long_text[ i ] = 0xFF;
-    }
-
-    print_uint8_hex(long_text, b.blockSize() * 2, "Plaintext: ");
-
-    // All test vectors are actually 16 bytes long, so double the block size   
-    // This breaks it down into the appropriate block size, may not be the best
-    // way, but it gets the job down
-    for ( unsigned int l = 0; l < ( b.blockSize() * 2 ); l += b.blockSize()) {
-        uint8_t* text = new uint8_t[ b.blockSize() ];
-        for ( unsigned int i = 0; i < b.blockSize(); i++ ) {
-            text[ i ] = long_text[ l + i ];
-        }
-
-        b.encrypt( text );
-
-        for ( unsigned int i = 0; i < b.blockSize(); i++ ) {
-            long_text[ l + i ] = text[ i ];
+    
+    for( unsigned int i = 0; i < b.blockSize(); i++ ){
+        char * p;
+        text[ i ] = (uint8_t) strtol( argv[ b.keySize() + 1 + i ], &p, 16 );
+        if( *p != 0 ){
+            cout << "Problem parsing string." << endl;
         }
     }
-
-    print_uint8_hex(long_text, b.blockSize() * 2, "Ciphertext: ");
+    print_uint8_hex(text, b.blockSize(), "Plaintext: ");
+    b.encrypt( text );
+    print_uint8_hex(text, b.blockSize(), "Ciphertext: ");
+    
 }
 
 Blowfish::Blowfish() {
@@ -63,7 +74,7 @@ uint32_t Blowfish::blockSize(){
 
 uint32_t Blowfish::keySize(){
     //56 is the maximum, this is variable
-    return 16;
+    return 8;
 }
 
 void Blowfish::setKey(uint8_t* key){
