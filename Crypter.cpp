17602d2560a16500e8c3e34ecacd93fb2344f3e6
Kevin Allison <kvnallsn@gmail.com>
2012-05-13 19:16:49 -0400

    Crypter encrypts files now

--- Crypter.cpp
+++ Crypter.cpp
@@ -23,42 +23,66 @@ void print_uint8_hex(uint8_t* buffer, uint32_t size, const char* label) {
 int main( int argc, char* argv[] ){
     Blowfish b;
 
-    if (argc != 4) {
+    if (argc <= 4) {
         cout << "usage: Crypter <input-file> <output-file> <key>" << endl;
         return 0;
     }
 
-    string str(argv[1]);
+    /*string str(argv[1]);
     istringstream is(str);
     uint32_t numIter = 0;
-    is >> numIter;
+    is >> numIter;*/
 
     uint8_t* key = new uint8_t[ b.keySize() ];
     
     for( unsigned int i = 0; i < b.keySize(); i++ ){
-        key[ i ] = 0x00;
+        char* p;
+        key[ i ] = (uint8_t)strtol( argv[ 3 + i ], &p, 16);
+        if (*p != 0) {
+            cout << "ADJUDICATE!" << endl;
+        }
     }
     
     print_uint8_hex(key, b.keySize(), "Key: ");
     b.setKey( key );
 
-
-    uint8_t* text;// = new uint8_t[ b.blockSize() ];
-    time_t start = time(NULL) * 1000;
-    for (unsigned int i = 0; i < numIter; i++) {
-        text = new uint8_t[ b.blockSize() ];
     
-        for( unsigned int i = 0; i < b.blockSize(); i++ ){
-            text[ i ] = 0x00;
-        }
+    FILE* input = fopen(argv[1], "r");
+    FILE* output = fopen(argv[2], "w+");
 
+    fseek(input, 0, SEEK_END);
+    long size = ftell(input);
+    rewind(input);
+
+    uint8_t* text;
+    time_t start = time(NULL) * 1000;
+    for (int i = 0; i < size; i+= b.blockSize()) {
+        text = new uint8_t[ b.blockSize() ];
+
+        fseek(input, i, SEEK_SET);
+        size_t readin = fread(text, b.blockSize(), 1, input); 
+
+        long s = ftell(input);
+
+        size_t delta = b.blockSize() - readin; 
+        if (delta != 0) {
+            for (unsigned int j = readin; j < b.blockSize(); j++) {
+                text[j] = 0x00;
+            }
+        }
+    
         b.encrypt( text );
+
+        fseek(output, i, SEEK_SET);
+        fwrite(text, b.blockSize(), 1, output);
+
         delete[] text;
     }
+
+    fclose(input);
+    fclose(output);
     time_t end = time(NULL) * 1000;
 
     cout << "Running Time: " << (end - start) << " msec" << endl;
-    //print_uint8_hex(text, b.blockSize(), "Plaintext");
-    //print_uint8_hex(text, b.blockSize(), "Ciphertext");
     
 }
